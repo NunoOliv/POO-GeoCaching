52faa1471176c3401927b502cd4e1fcbc77d9c3a
NunoOliv <nuno.oliv.94@gmail.com>
2015-04-02 14:24:07 +0100

    Revert "Revert "Revert "Revert "Criação das Caches""""
    This reverts commit d03d0a5007a6e86c27c55ef85d21790f3c73276a.

---
+++ GeoCachingPOO/src/Data/Coords.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Data;
+
+/**
+ *
+ * @author Nuno Oliveira
+ */
+public class Coords {
+
+    public Coords(float latitude, float longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+    
+    private float latitude;
+    private float longitude;
+
+    /**
+     * @return the latitude
+     */
+    public float getLatitude() {
+        return latitude;
+    }
+
+    /**
+     * @param latitude the latitude to set
+     */
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
+    }
+
+    /**
+     * @return the longitude
+     */
+    public float getLongitude() {
+        return longitude;
+    }
+
+    /**
+     * @param longitude the longitude to set
+     */
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
+
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Coords other = (Coords) obj;
+        if (Float.floatToIntBits(this.latitude) != Float.floatToIntBits(other.latitude)) {
+            return false;
+        }
+        if (Float.floatToIntBits(this.longitude) != Float.floatToIntBits(other.longitude)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
