7e37bc7b4d17c8cad0eec19869532b5e8af1ab45
Rafael Antunes <rafael.uminho@hotmail.com>
2015-04-02 16:10:15 +0100

    Registar feito!

--- GeoCachingPOO/src/Business/Core.java
+++ GeoCachingPOO/src/Business/Core.java
@@ -2,13 +2,7 @@ package Business;
 
 import Data.User;
 import Data.UserList;
-import Exceptions.CamposInvalidosException;
-import Exceptions.DataInvalidaException;
-import Exceptions.EmailInvalidoException;
-import Exceptions.EmailJaExisteException;
-import Exceptions.GeneroInvalidoException;
-import Exceptions.PasswordMissmatchException;
-import Exceptions.UserNaoExisteException;
+import Exceptions.*;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.logging.Level;
@@ -24,13 +18,13 @@ public class Core {
 
     private UserList userL;
     private User sessao;
-    
-    public Core(){
+
+    public Core() {
         userL = new UserList();
-        sessao=null;
+        sessao = null;
     }
-    
-    public void inicialize(){
+
+    public void inicialize() {
         LocalDate dn1 = LocalDate.of(1994, Month.OCTOBER, 27);
         LocalDate dn2 = LocalDate.of(1994, Month.JUNE, 10);
         LocalDate dn3 = LocalDate.of(1994, Month.MAY, 4);
@@ -48,13 +42,54 @@ public class Core {
             Logger.getLogger(Core.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    public void login(String mail,String pass) throws EmailInvalidoException, CamposInvalidosException, UserNaoExisteException, PasswordMissmatchException{
+
+    public void login(String mail, String pass) throws EmailInvalidoException, CamposInvalidosException, UserNaoExisteException, PasswordMissmatchException {
         userL.existeUser(mail);
-        if(userL.checkPass(mail, pass)){
-            sessao=userL.getUser(mail);
-        }else{
+        if (userL.checkPass(mail, pass)) {
+            sessao = userL.getUser(mail);
+        } else {
             throw new PasswordMissmatchException();
         }
     }
+
+    public void registar(String mail, String pass, String nome, String genero, String morada, int dia, int mes, int ano) throws CamposInvalidosException, EmailJaExisteException, GeneroInvalidoException, DataInvalidaException, EmailInvalidoException {
+        checkMail(mail);
+        LocalDate dn = buildDate(dia, mes, ano);
+        if (genero.equals("M") || genero.equals("m")) {
+            genero = "Masculino";
+        }
+        if (genero.equals("F") || genero.equals("f")) {
+            genero = "Feminino";
+        }
+        if (!genero.equals("Masculino") && !genero.equals("Feminino")) {
+            throw new GeneroInvalidoException();
+        }
+        userL.addUser(mail, pass, nome, genero, morada, dn);
+    }
+
+    public void checkMail(String mail) throws EmailInvalidoException {
+        if (mail == null) {
+            throw new EmailInvalidoException();
+        }
+        if (mail.equals("")) {
+            throw new EmailInvalidoException();
+        }
+        if (!mail.matches("(.+)@(.+)\\.(.+)")) {
+            throw new EmailInvalidoException();
+        }
+    }
+
+    public LocalDate buildDate(int dia, int mes, int ano) throws CamposInvalidosException {
+        if (dia > 31 || dia < 1) {
+            throw new CamposInvalidosException();
+        }
+        if (mes > 12 || mes < 1) {
+            throw new CamposInvalidosException();
+        }
+        if (ano > LocalDate.now().getYear()) {
+            throw new CamposInvalidosException();
+        }
+        return LocalDate.of(ano, mes, dia);
+    }
+
 }
